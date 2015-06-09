418e38612bc573b77688aafbc920a90a15bd8fbd
Carlos Gonzalez Juarez <carlosgj94@gmail.com>
2015-06-09 21:15:44 +0200

    bug de encima del cuadrado, triple salto, algun color, la interfaz, los records...

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EnemyController : MonoBehaviour {
+    public Text puntuacion;
+    public Text record;
+    private int puntos = 0;
+    private int recordPuntos = 0;
+
+    private bool movement=false;
+	// Use this for initialization
+	void Start () {
+        puntuacion.text = "Score: 0";
+        record.text = "Record: 0";
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        
+        if (!movement)
+        {
+            movement = true;
+            Vector2 force = new Vector2(0 , 0.4f);
+            this.GetComponent<Rigidbody2D>().AddForce(force, ForceMode2D.Impulse);
+        }
+  
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "wallL")
+        {
+            Vector2 force = new Vector2(0.2f, 0);
+            this.GetComponent<Rigidbody2D>().AddForce(force, ForceMode2D.Impulse);
+        }
+        if (coll.gameObject.tag == "wallR")
+        {
+            Vector2 force = new Vector2(-0.2f, 0);
+            this.GetComponent<Rigidbody2D>().AddForce(force, ForceMode2D.Impulse);
+        }
+        if (coll.gameObject.tag == "Player")
+        {
+            puntos++;
+            puntuacion.text = "Score: " + puntos + " ";
+            if (puntos >= recordPuntos)
+            {
+                recordPuntos = puntos;
+                record.text = "Record: " + puntos + " ";
+            }
+        }
+
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "ground")
+        {
+            movement = false;
+            puntos = 0;
+            puntuacion.text = "Score: " + puntos + " ";
+        }
+    }
+}
--- Assets/Scripts/HulkController.cs
+++ Assets/Scripts/HulkController.cs
@@ -1,27 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class HulkController : MonoBehaviour {
-	float speed = 2;
+	float speed = 1.5f;
 	[SerializeField] private float speedJumpUp = 1;
 
 	private bool grounded = true;
 	bool facingRight = false;
 
 	private bool secondJump = true;
-	public float momment=0;
+	private float momment=0;
+
+    private bool thirdJump = false;
+    private float mommentThird = 0;
+    
 	// Use this for initialization
 	void Start () {
-	
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		float direction = Input.GetAxisRaw ("Horizontal");
 		if (Input.touchCount > 0) {
-			if (Input.touches [0].deltaPosition.y > 0)
+            Touch touch = Input.GetTouch(0);
+            if (touch.position.x > Screen.width / 2)
 				direction = 1;
-			if (Input.touches [0].deltaPosition.y < 0)
+            if (touch.position.x < Screen.width / 2)
 				direction = -1;
 		}
 		if (direction < 0 && facingRight)
@@ -30,15 +36,22 @@ public class HulkController : MonoBehaviour {
 			Flip ();
 		if (direction != 0 && grounded) {
 			grounded=false;
+            thirdJump = false;
 			Vector2 jumpForceUp = new Vector2 (speed * direction, speedJumpUp);
 			this.GetComponent<Rigidbody2D> ().AddForce (jumpForceUp, ForceMode2D.Impulse);
 			momment = Time.time;
 		}
-		if (direction != 0 && !grounded && Time.time-momment>0.3 && secondJump) {
+		if (direction != 0 && !grounded && Time.time-momment>0.38 && secondJump) {
 			secondJump= false;
-			Vector2 jumpForceUp = new Vector2 ((speed/2) * direction, 3);
+			Vector2 jumpForceUp = new Vector2 ((speed/0.6f) * direction, 3);
 			this.GetComponent<Rigidbody2D> ().AddForce (jumpForceUp, ForceMode2D.Impulse);
 		}
+        if (direction != 0 && !grounded && !secondJump && thirdJump && Time.time - mommentThird > 0.45 && mommentThird-momment>0.3)
+        {
+            thirdJump = false;
+            Vector2 jumpForceUp = new Vector2((speed / 1) * direction, 3);
+            this.GetComponent<Rigidbody2D>().AddForce(jumpForceUp, ForceMode2D.Impulse);
+        }
 	}
 	void Flip()
 	{
@@ -56,6 +69,21 @@ public class HulkController : MonoBehaviour {
 			grounded = true;
 			momment=0;
 			secondJump= true;
-		}
+        }
+        if (coll.gameObject.tag == "Enemy")
+        {
+            thirdJump = true;
+            mommentThird = Time.time;
+        }
+        if (coll.gameObject.tag == "wallL")
+        {
+            Vector2 force = new Vector2(0.3f, 0);
+            this.GetComponent<Rigidbody2D>().AddForce(force, ForceMode2D.Impulse);
+        }
+        if (coll.gameObject.tag == "wallR")
+        {
+            Vector2 force = new Vector2(-0.3f, 0);
+            this.GetComponent<Rigidbody2D>().AddForce(force, ForceMode2D.Impulse);
+        }
 	}
 }
